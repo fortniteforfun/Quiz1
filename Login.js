09243b99c32b36d1ec4aa0f97ae3b29e9555d67b
fortniteforfun <63515999+fortniteforfun@users.noreply.github.com>
2021-11-17 16:18:24 -0500

    Delete Login.js

--- Login.js
+++
@@ -1,7 +0,0 @@
-function adduser(){
-    player1=document.getElementById("player1nameinput").value
-    player2=document.getElementById("player2nameinput").value
-    localStorage.setItem("player1name",player1)
-    localStorage.setItem("player2name",player2)
-    window.location.replace("Game.html")
-}
